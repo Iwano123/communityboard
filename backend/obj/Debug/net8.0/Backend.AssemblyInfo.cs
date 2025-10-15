9f18749586d8ab4ba7e8d9e6af3f96d580b1b8de
Iwano123 <korkisiwan@gmail.com>
2025-10-15 22:54:23 +0200

    fixed acl issues

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c23b03282bdb64c1650522e9640647a663a2d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6147177d183c29699064ab947a61f04dd668d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
